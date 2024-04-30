8d254d8a97e5e456ae1e612a20110e144c55fb07
evildev <evildev@evildev.ru>
2024-04-30 15:32:08 +0700

    done

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/canban',
-  publicPath: '/',
   plugins: [vue()],
-  build: {
-    assetsDir: '/',
-  },
 });
